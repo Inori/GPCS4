296b172a30c7b27d90d76ee576d57fa0a4f2d37d
Asuka <bingkan@outlook.com>
2019-07-05 21:03:11 +0800

    change main thread stack size

--- GPCS4/Emulator/GameThread.cpp
+++ GPCS4/Emulator/GameThread.cpp
@@ -2,6 +2,7 @@
 #include "TLSHandler.h"
 #include "Platform/UtilThread.h"
 
+#define PS4_MAIN_THREAD_STACK_SIZE (1024 * 1024 * 5)
 
 CGameThread::CGameThread(void* pFunc, void* pArg):
 	m_pFunc(pFunc),
@@ -36,11 +37,26 @@ bool CGameThread::Start()
 		m_pStartParam = std::make_unique<PTHREAD_START_PARAM>();
 		m_pStartParam->pThis = this;
 
-		int nRet = pthread_create(&m_nTid, NULL, CGameThread::ThreadFunc, m_pStartParam.get());
+		pthread_attr_t attr;
+		int nRet = pthread_attr_init(&attr);
 		if (nRet != 0)
 		{
 			break;
 		}
+
+		nRet = pthread_attr_setstacksize(&attr, PS4_MAIN_THREAD_STACK_SIZE);
+		if (nRet != 0)
+		{
+			break;
+		}
+
+		nRet = pthread_create(&m_nTid, &attr, CGameThread::ThreadFunc, m_pStartParam.get());
+		if (nRet != 0)
+		{
+			break;
+		}
+
+		pthread_attr_destroy(&attr);
 
 		bRet = true;
 	} while (false);
