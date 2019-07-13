ee3e26148a161e1c42f208a9943777d39eb0092b
Asuka <bingkan@outlook.com>
2019-07-14 02:39:20 +0800

    update vm function implement

--- GPCS4/Platform/UtilMemory.cpp
+++ GPCS4/Platform/UtilMemory.cpp
@@ -9,12 +9,62 @@ namespace UtilMemory
 #include <Windows.h>
 #undef WIN32_LEAN_AND_MEAN
 
+inline uint GetProtectFlag(uint nOldFlag)
+{
+	uint nNewFlag = 0;
+	do 
+	{
+		if (nOldFlag & VMPF_NOACCESS)
+		{
+			nNewFlag |= PAGE_NOACCESS;
+			break;
+		}
+
+		if (nOldFlag & VMPF_READ)
+		{
+			nNewFlag |= PAGE_READONLY;
+		}
+
+		if (nOldFlag & VMPF_WRITE)
+		{
+			nNewFlag |= PAGE_READWRITE;
+		}
+
+		if (nOldFlag & VMPF_EXECUTE)
+		{
+			nNewFlag |= PAGE_EXECUTE_READWRITE;
+		}
+
+	} while (false);
+	return nNewFlag;
+}
+
+inline uint GetTypeFlag(uint nOldFlag)
+{
+	uint nNewFlag = 0;
+	do
+	{
+		if (nOldFlag & VMAT_RESERVE)
+		{
+			nNewFlag |= MEM_RESERVE;
+			break;
+		}
+
+		if (nOldFlag & VMAT_COMMIT)
+		{
+			nNewFlag |= MEM_COMMIT;
+		}
+
+	} while (false);
+	return nNewFlag;
+}
+
 void* VMMap(size_t nSize, uint nProtectFlag)
 {
 	void* pAddr = NULL;
 	do 
 	{
-		pAddr = VirtualAlloc(NULL, nSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+		pAddr = VirtualAlloc(NULL, nSize, MEM_RESERVE | MEM_COMMIT, GetProtectFlag(nProtectFlag));
 	} while (false);
 	return pAddr;
 }
@@ -23,7 +73,7 @@ void* VMMapEx(void* pAddr, size_t nSize, uint nProtectFlag, uint nType)
 {
 	do
 	{
-		pAddr = VirtualAlloc(pAddr, nSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+		pAddr = VirtualAlloc(pAddr, nSize, GetTypeFlag(nType), GetProtectFlag(nProtectFlag));
 	} while (false);
 	return pAddr;
 }
