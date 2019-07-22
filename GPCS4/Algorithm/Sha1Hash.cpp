37c6f5e2f53cbd2b540969e2ca0896537ce7ed93
Asuka <bingkan@outlook.com>
2019-07-22 20:00:34 +0800

    add Algorithm and sha1

---
+++ GPCS4/Algorithm/Sha1Hash.cpp
@@ -0,0 +1,54 @@
+#include "sha1.h"
+#include "Sha1Hash.h"
+
+namespace algo 
+{
+  
+  std::string Sha1Hash::toString() const 
+  {
+    static const char nibbles[]
+      = { '0', '1', '2', '3', '4', '5', '6', '7',
+          '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    
+    std::string result;
+    result.resize(2 * m_digest.size());
+    
+    for (uint32_t i = 0; i < m_digest.size(); i++) 
+	{
+      result.at(2 * i + 0) = nibbles[(m_digest[i] >> 4) & 0xF];
+      result.at(2 * i + 1) = nibbles[(m_digest[i] >> 0) & 0xF];
+    }
+    
+    return result;
+  }
+  
+  
+  Sha1Hash Sha1Hash::compute(
+    const void*     data,
+          size_t    size) 
+  {
+    Sha1Data chunk = { data, size };
+    return compute(1, &chunk);
+  }
+  
+  
+  Sha1Hash Sha1Hash::compute(
+          size_t    numChunks,
+    const Sha1Data* chunks) 
+  {
+    Sha1Digest digest;
+    
+    SHA1_CTX ctx;
+    SHA1Init(&ctx);
+    
+    for (size_t i = 0; i < numChunks; i++) 
+	{
+      auto ptr = reinterpret_cast<const uint8_t*>(chunks[i].data);
+      SHA1Update(&ctx, ptr, chunks[i].size);
+    }
+
+    SHA1Final(digest.data(), &ctx);
+    return Sha1Hash(digest);
+  }
+  
+}
---
+++ GPCS4/Algorithm/Sha1Hash.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <array>
+#include <cstring>
+#include <string>
+
+namespace algo {
+  
+  using Sha1Digest = std::array<uint8_t, 20>;
+
+  struct Sha1Data {
+    const void* data;
+    size_t      size;
+  };
+  
+  class Sha1Hash {
+    
+  public:
+    
+    Sha1Hash() { }
+    Sha1Hash(const Sha1Digest& digest)
+    : m_digest(digest) { }
+    
+    std::string toString() const;
+    
+    uint32_t dword(uint32_t id) const 
+	{
+      return uint32_t(m_digest[4u + id + 0u]) <<  0u
+           | uint32_t(m_digest[4u + id + 1u]) <<  8u
+           | uint32_t(m_digest[4u + id + 2u]) << 16u
+           | uint32_t(m_digest[4u + id + 3u]) << 24u;
+    }
+    
+    bool operator == (const Sha1Hash& other) const 
+	{
+      return !std::memcmp(
+        this->m_digest.data(),
+        other.m_digest.data(),
+        other.m_digest.size());
+    }
+    
+    static Sha1Hash compute(
+      const void*     data,
+            size_t    size);
+    
+    static Sha1Hash compute(
+            size_t    numChunks,
+      const Sha1Data* chunks);
+    
+    template<typename T>
+    static Sha1Hash compute(const T& data) 
+	{
+      return compute(&data, sizeof(T));
+    }
+
+  private:
+    
+    Sha1Digest m_digest;
+    
+  };
+  
+}
