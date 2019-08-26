a87418b1608f00893bc427a43d400b3cac9d1a7f
Asuka <bingkan@outlook.com>
2019-08-26 23:54:24 +0800

    Add empty GveSwapChain

---
+++ GPCS4/Graphic/Gve/GveSwapChain.cpp
@@ -0,0 +1,19 @@
+#include "GveSwapChain.h"
+
+namespace gve
+{;
+
+
+GveSwapChain::GveSwapChain(VkSurfaceKHR windowSurface):
+	m_windowSurface(windowSurface)
+{
+
+}
+
+GveSwapChain::~GveSwapChain()
+{
+}
+
+
+
+}  // namespace gve
--- GPCS4/Graphic/Gve/GveSwapChain.h
+++ GPCS4/Graphic/Gve/GveSwapChain.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "GveCommon.h"
+
+namespace gve
+{;
+
+class GveSwapChain
+{
+public:
+	GveSwapChain(VkSurfaceKHR windowSurface);
+	~GveSwapChain();
+
+private:
+	VkSurfaceKHR m_windowSurface;
+};
+
+
+}  // namespace gve
