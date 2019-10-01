fd996c2d79f7886fb17033d566bc89cd47010f0c
Asuka <bingkan@outlook.com>
2019-10-01 22:50:21 +0800

    Add GcnRegSwizzle and GcnRegMask class

--- GPCS4/Graphic/Pssl/GCNDecoder.h
+++ GPCS4/Graphic/Pssl/GCNDecoder.h
@@ -8,6 +8,114 @@
 namespace pssl
 {;
 
+
+/**
+ * \brief Component swizzle
+ *
+ * Maps vector components to
+ * other vector components.
+ */
+class GcnRegSwizzle 
+{
+
+public:
+
+	GcnRegSwizzle() { }
+	GcnRegSwizzle(uint32_t x, uint32_t y, uint32_t z, uint32_t w)
+		: m_mask((x << 0) | (y << 2) | (z << 4) | (w << 6)) { }
+
+	uint32_t operator [] (uint32_t id) const 
+	{
+		return (m_mask >> (id + id)) & 0x3;
+	}
+
+	bool operator == (const GcnRegSwizzle& other) const { return m_mask == other.m_mask; }
+	bool operator != (const GcnRegSwizzle& other) const { return m_mask != other.m_mask; }
+
+private:
+
+	uint8_t m_mask = 0;
+
+};
+
+
+/**
+ * \brief Component mask
+ *
+ * Enables access to certain
+ * subset of vector components.
+ */
+class GcnRegMask 
+{
+
+public:
+
+	GcnRegMask() { }
+	GcnRegMask(uint32_t mask) : m_mask(mask) { }
+	GcnRegMask(bool x, bool y, bool z, bool w)
+		: m_mask((x ? 0x1 : 0) | (y ? 0x2 : 0)
+			| (z ? 0x4 : 0) | (w ? 0x8 : 0)) { }
+
+	bool operator [] (uint32_t id) const 
+	{
+		return (m_mask >> id) & 1;
+	}
+
+	uint32_t popCount() const 
+	{
+		const uint8_t n[16] = { 0, 1, 1, 2, 1, 2, 2, 3,
+								1, 2, 2, 3, 2, 3, 3, 4 };
+		return n[m_mask & 0xF];
+	}
+
+	uint32_t firstSet() const 
+	{
+		const uint8_t n[16] = { 4, 0, 1, 0, 2, 0, 1, 0,
+								3, 0, 1, 0, 2, 0, 1, 0 };
+		return n[m_mask & 0xF];
+	}
+
+	uint32_t minComponents() const 
+	{
+		const uint8_t n[16] = { 0, 1, 2, 2, 3, 3, 3, 3,
+								4, 4, 4, 4, 4, 4, 4, 4 };
+		return n[m_mask & 0xF];
+	}
+
+	bool operator == (const GcnRegMask& other) const { return m_mask == other.m_mask; }
+	bool operator != (const GcnRegMask& other) const { return m_mask != other.m_mask; }
+
+	GcnRegMask& operator |= (const GcnRegMask& other) 
+	{
+		m_mask |= other.m_mask;
+		return *this;
+	}
+
+	static GcnRegMask firstN(uint32_t n) 
+	{
+		return GcnRegMask(n >= 1, n >= 2, n >= 3, n >= 4);
+	}
+
+	static GcnRegMask select(uint32_t n) 
+	{
+		return GcnRegMask(n == 0, n == 1, n == 2, n == 3);
+	}
+
+	std::string maskString() const {
+		std::string out = "";
+		out += (m_mask & 0x1) ? "x" : "";
+		out += (m_mask & 0x2) ? "y" : "";
+		out += (m_mask & 0x4) ? "z" : "";
+		out += (m_mask & 0x8) ? "w" : "";
+		return out;
+	}
+
+private:
+
+	uint8_t m_mask = 0;
+
+};
+
 class GCNCodeSlice {
 
 public:
