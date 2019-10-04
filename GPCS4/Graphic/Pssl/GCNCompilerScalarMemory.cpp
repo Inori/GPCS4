9466b34e582411cf3f6376dbba88086acd3fe80b
Inori <bingkan@outlook.com>
2019-10-05 03:37:04 +0800

    Support S_BUFFER_LOAD_DWORDxN

--- GPCS4/Graphic/Pssl/GCNCompilerScalarMemory.cpp
+++ GPCS4/Graphic/Pssl/GCNCompilerScalarMemory.cpp
@@ -1,5 +1,6 @@
 #include "GCNCompiler.h"
 
+
 namespace pssl
 {;
 
@@ -28,7 +29,12 @@ void GCNCompiler::emitScalarMemory(GCNInstruction& ins)
 void GCNCompiler::emitScalarMemRd(GCNInstruction& ins)
 {
 	auto inst = asInst<SISMRDInstruction>(ins);
+
 	auto op = inst->GetOp();
+	uint32_t startRegIdx = inst->GetSRidx();
+	uint32_t offset = inst->GetOffset() << 2;
+
+	uint32_t uniformFloatPtrId = m_module.defFloatPointerType(32, spv::StorageClassUniform);
 
 	switch (op)
 	{
@@ -43,6 +49,25 @@ void GCNCompiler::emitScalarMemRd(GCNInstruction& ins)
 	case SISMRDInstruction::S_BUFFER_LOAD_DWORDX4:
 	case SISMRDInstruction::S_BUFFER_LOAD_DWORDX8:
 	case SISMRDInstruction::S_BUFFER_LOAD_DWORDX16:
+	{
+		uint32_t regCount = 2 << ((uint32_t)op - 10 + 1);
+
+		std::vector<SpirvRegisterValue> valueArray;
+		for (uint32_t i = 0; i != regCount; ++i)
+		{
+			uint32_t dwordOffset = offset / sizeof(uint32_t) + i;
+			uint32_t offsetId = m_module.constu32(dwordOffset);
+			std::array<uint32_t, 2> indices = { m_module.constu32(0), offsetId };
+			uint32_t srcId = m_module.opAccessChain(
+				uniformFloatPtrId,
+				m_uboId, 
+				indices.size(), indices.data());
+			auto value = emitValueLoad({ SpirvScalarType::Float32, 1, srcId });
+			valueArray.emplace_back(SpirvScalarType::Float32, 1, value.id);
+		}
+
+		emitSgprArrayStore(startRegIdx, valueArray.data(), valueArray.size());
+	}
 		break;
 	default:
 		break;
