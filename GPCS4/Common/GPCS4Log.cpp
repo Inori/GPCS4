cd68886ebd2995894901ed6bfc8b10d96da139bf
sgqy <sgqyzh@gmail.com>
2019-11-20 01:19:40 +0800

    switch logger to spdlog

--- GPCS4/Common/GPCS4Log.cpp
+++ GPCS4/Common/GPCS4Log.cpp
@@ -1,63 +1,84 @@
 
 #include "GPCS4Log.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
 
-#define LOG_STR_BUFFER_LEN 1024 
-#define LOG_TAG "[Asuka]"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/msvc_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include <memory>
+#include <cstdio>
+
+#define LOG_STR_BUFFER_LEN 2000
 
 #ifdef GPCS4_WINDOWS
 
 #include <Windows.h>
 
-void LogPrint(const char* szLevel, const char* szFunction, int nLine, const char* szFormat, ...)
+static std::unique_ptr<spdlog::logger> logger;
+
+void InitLogging()
 {
-	char szDbgStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
-	char szTempStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
-	unsigned int nTid = 0;
-	va_list   stArgList;
+	auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	console_sink->set_level(spdlog::level::trace); // message generating filter
+	console_sink->set_pattern("[%T.%f][%t][%^%l%$]%v");
 
-	nTid = GetCurrentThreadId();
-	va_start(stArgList, szFormat);
+	auto msvc_sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+	msvc_sink->set_level(spdlog::level::trace); // message generating filter
+	msvc_sink->set_pattern("[Asuka:%t][%l]%v");
 
-	vsprintf_s(szTempStr, LOG_STR_BUFFER_LEN, szFormat, stArgList);
-	sprintf_s(szDbgStr, LOG_STR_BUFFER_LEN, "%s:%s:%d:%s:%04d:%s\n",
-		LOG_TAG,
-		szLevel,
-		nTid,
-		szFunction,
-		nLine,
-		szTempStr);
-
-	va_end(stArgList);
-
-	OutputDebugStringA(szDbgStr);
-	//printf(szDbgStr);
+	logger.reset(new spdlog::logger("stdout_msvc", { console_sink, msvc_sink }));
+	logger->set_level(spdlog::level::trace); // message showing filter
 }
 
-void LogAssert(const char* szLevel, const char* szFunction, int nLine, const char* szFormat, ...)
+void LogPrint(LogLevel nLevel, const char* szFunction, const char* szSourcePath, int nLine, const char* szFormat, ...)
 {
-	char szDbgStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
-	char szTempStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
-	unsigned int nTid = 0;
-	va_list   stArgList;
-
-	nTid = GetCurrentThreadId();
+	// generate format string
+	va_list stArgList;
 	va_start(stArgList, szFormat);
-
+	char szTempStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
 	vsprintf_s(szTempStr, LOG_STR_BUFFER_LEN, szFormat, stArgList);
-	sprintf_s(szDbgStr, LOG_STR_BUFFER_LEN, "%s:%s:%d:%s:%04d:%s\n",
-		LOG_TAG,
-		szLevel,
-		nTid,
-		szFunction,
-		nLine,
-		szTempStr);
-
 	va_end(stArgList);
-	MessageBoxA(NULL, szDbgStr, "Error", MB_OK | MB_ICONERROR);
+
+
+	switch (nLevel)
+	{
+	case LogLevel::debug:
+		logger->debug("{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	case LogLevel::trace:
+		logger->trace("{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	case LogLevel::fixme:
+		logger->warn("<FIXME>{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	case LogLevel::warning:
+		logger->warn("{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	case LogLevel::error:
+		logger->error("{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	case LogLevel::sce_trace:
+		logger->trace("<SCE>{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	case LogLevel::sce_graphic:
+		logger->trace("<GRAPH>{}({}): {}", szFunction, nLine, szTempStr);
+		break;
+	}
+}
+
+void LogAssert(const char* szExpression, const char* szFunction, const char* szSourcePath, int nLine, const char* szFormat, ...)
+{
+	// generate format string
+	va_list stArgList;
+	va_start(stArgList, szFormat);
+	char szTempStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
+	vsprintf_s(szTempStr, LOG_STR_BUFFER_LEN, szFormat, stArgList);
+	char szMsgBoxStr[LOG_STR_BUFFER_LEN + 1] = { 0 };
+	sprintf_s(szMsgBoxStr, "[Assert]: %s\n[Cause]: %s\n[Path]: %s(%d): %s", szExpression, szTempStr, szSourcePath, nLine, szFunction);
+	va_end(stArgList);
+
+	logger->critical("{}({}): [Assert: {}] {}", szFunction, nLine, szExpression, szTempStr);
+	MessageBoxA(NULL, szMsgBoxStr, "Assertion Fail", MB_OK | MB_ICONERROR);
 	exit(-1);
 }
 
--- GPCS4/Common/GPCS4Log.h
+++ GPCS4/Common/GPCS4Log.h
@@ -2,46 +2,50 @@
 
 #include "GPCS4Common.h"
 
+void InitLogging();
 
-#define LOG_LEVEL_DEBUG "DEBUG"
-#define LOG_LEVEL_TRACE "TRACE"
-#define LOG_LEVEL_FIXME "FIXME"
-#define LOG_LEVEL_WARNING "WARNING"
-#define LOG_LEVEL_ERROR "ERROR"
-#define LOG_LEVEL_SCE_TRACE "SCE-TRACE"
-#define LOG_LEVEL_SCE_GRAPHIC "SCE-GRAPHIC"
+static enum LogLevel : int
+{
+	trace,
+	sce_trace,
+	debug,
+	sce_graphic,
+	fixme,
+	warning,
+	error,
+};
 
 //do not use these directly
-void LogPrint(const char* szLevel, const char* szFunction, int nLine, const char* szFormat, ...);
-void LogAssert(const char* szLevel, const char* szFunction, int nLine, const char* szFormat, ...);
-#define _LOG_PRINT_(level, format, ...) LogPrint(level, __FUNCTION__, __LINE__, format, __VA_ARGS__)
-#define _LOG_ASSERT_(level, format, ...) LogAssert(level, __FUNCTION__, __LINE__, format, __VA_ARGS__)
+void LogPrint(LogLevel nLevel, const char* szFunction, const char* szSourcePath, int nLine, const char* szFormat, ...);
+void LogAssert(const char* szExpression, const char* szFunction, const char* szSourcePath, int nLine, const char* szFormat, ...);
+#define _LOG_PRINT_(level, format, ...) LogPrint(level, __FUNCTION__, __FILE__, __LINE__, format, __VA_ARGS__)
+#define _LOG_ASSERT_(expr, format, ...) LogAssert(expr, __FUNCTION__, __FILE__, __LINE__, format, __VA_ARGS__)
 
 
 #ifdef GPCS4_DEBUG
 
 // for debug print
-#define LOG_DEBUG(format, ...)	_LOG_PRINT_(LOG_LEVEL_DEBUG, format, __VA_ARGS__);
+#define LOG_DEBUG(format, ...)	_LOG_PRINT_(LogLevel::debug, format, __VA_ARGS__);
 // for trace calling
-#define LOG_TRACE(format, ...)	_LOG_PRINT_(LOG_LEVEL_TRACE, format, __VA_ARGS__);
+#define LOG_TRACE(format, ...)	_LOG_PRINT_(LogLevel::trace, format, __VA_ARGS__);
 // should be fixed, but without fix, the program should run, treat this as TODO
-#define LOG_FIXME(format, ...)	_LOG_PRINT_(LOG_LEVEL_FIXME, format, __VA_ARGS__);
+#define LOG_FIXME(format, ...)	_LOG_PRINT_(LogLevel::fixme, format, __VA_ARGS__);
 // should be fixed, without this, program can run, but behaves unexpected
-#define LOG_WARN(format, ...)	_LOG_PRINT_(LOG_LEVEL_WARNING, format, __VA_ARGS__);
+#define LOG_WARN(format, ...)	_LOG_PRINT_(LogLevel::warning, format, __VA_ARGS__);
 // critical error, program can't go on
-#define LOG_ERR(format, ...)	_LOG_PRINT_(LOG_LEVEL_ERROR, format, __VA_ARGS__);
+#define LOG_ERR(format, ...)	_LOG_PRINT_(LogLevel::error, format, __VA_ARGS__);
 // critical error, log then pop up a window then exit process
 #define LOG_ASSERT(expression, format, ...) (void)(                 \
             (!!(expression)) ||                                     \
-            (_LOG_ASSERT_(LOG_LEVEL_ERROR, format, __VA_ARGS__), 0) \
+            (_LOG_ASSERT_(#expression, format, __VA_ARGS__), 0) \
         )
 
 // only use to trace sce module export functions
 // to trace other functions, use LOG_TRACE
-#define LOG_SCE_TRACE(format, ...)	_LOG_PRINT_(LOG_LEVEL_SCE_TRACE, format, __VA_ARGS__);
+#define LOG_SCE_TRACE(format, ...)	_LOG_PRINT_(LogLevel::sce_trace, format, __VA_ARGS__);
 
 // only use to trace graphic calls, mostly in libVideoOut and libGnmDriver
-#define LOG_SCE_GRAPHIC(format, ...)	_LOG_PRINT_(LOG_LEVEL_SCE_GRAPHIC, format, __VA_ARGS__);
+#define LOG_SCE_GRAPHIC(format, ...)	_LOG_PRINT_(LogLevel::sce_graphic, format, __VA_ARGS__);
 
 
 // not really implemented
@@ -61,4 +65,4 @@ void LogAssert(const char* szLevel, const char* szFunction, int nLine, const cha
 #define LOG_SCE_GRAPHIC(format, ...)	
 #define LOG_SCE_DUMMY_IMPL()
 
-#endif  //ENABLE_DEBUG
+#endif // GPCS4_DEBUG
