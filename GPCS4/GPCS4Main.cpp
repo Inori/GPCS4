464adc40f261d439a0933685ed687345203bba72
Asuka <bingkan@outlook.com>
2019-11-24 11:18:11 +0800

    Delete test code

--- GPCS4/GPCS4Main.cpp
+++ GPCS4/GPCS4Main.cpp
@@ -27,8 +27,6 @@ int main(int argc, char *argv[])
 			break;
 		}
 
-		// TODO: for testing only. remember to remove this
-
 		CTLSHandlerWin tlsHandler = {};
 
 		CLinker linker      = {*CSceModuleSystem::GetInstance()};
@@ -41,23 +39,15 @@ int main(int argc, char *argv[])
 		}
 
 		auto modSystem = CSceModuleSystem::GetInstance();
-		//auto ptr = modSystem->FindFunction("libkernel", "libkernel", 0xF41703CA43E6A352);
 		
-
 
 		if(!pEmulator->Run(*ebootModule))
 		{
 			break;
 		}
 
-		// if (!pEmulator->LoadEboot(szEboot))
-		//{
-		//	break;
-		//}
-
-		// pEmulator->Run();
-
-		// pEmulator->Unit();
+		pEmulator->Unit();
+		
 		nRet = 0;
 	} while (false);
 
