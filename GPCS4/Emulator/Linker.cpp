a560008d432e687481613a370bc60de3f4759d95
Nick Renieris <velocityra@gmail.com>
2019-12-01 12:07:36 +0200

    linker: Log base64 form of NID on unknown calls

--- GPCS4/Emulator/Linker.cpp
+++ GPCS4/Emulator/Linker.cpp
@@ -96,7 +96,7 @@ bool CLinker::resolveSymbol(MemoryMappedModule const &mod,
 			{
 				// NOTE: Something is wrong with va_args and u64 values, so print NID as 2 u32
 				formatString =
-					"Unknown Function nid 0x%08x%08x from lib:%s is called: 0x%08x";
+					"Unknown Function nid 0x%08x%08x (\"%s\") from lib:%s is called at 0x%08x";
 			}
 			else
 			{
@@ -104,9 +104,12 @@ bool CLinker::resolveSymbol(MemoryMappedModule const &mod,
 					"Function nid 0x%08x%08x from lib:%s is called";
 			}
 
+			auto nidString = info->symbolName.substr(0, 11);
+
 			auto msg = UtilString::Format(formatString,
 										  info->nid >> 32,
 										  info->nid,
+										  nidString.c_str(),
 										  info->libraryName.c_str());
 
 			auto stubMgr  = GetFuncStubManager();
