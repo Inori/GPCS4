d33953a668488655d2903ef5e2af2b1d938d5684
Asuka <bingkan@outlook.com>
2020-01-22 19:55:30 +0800

    delete PSSL_SPIRV_CROSS_DEBUG switch

--- GPCS4/Graphic/Pssl/GCNCompiler.cpp
+++ GPCS4/Graphic/Pssl/GCNCompiler.cpp
@@ -551,14 +551,11 @@ void GCNCompiler::emitDclImmConstBuffer(const GcnResourceBuffer& res)
 	// set the ArrayStride to 16 no matter what type the element is.
 	// This is std140 standard, but what we should use is std430
 	// We should specify the correct stride, for a float array, it's sizeof(float) == 4 .
-
-#ifndef PSSL_SPIRV_CROSS_DEBUG
 	m_module.decorateArrayStride(arrayId, 4);
-#else
-	// spirv-cross doesn't support buffer block expressed as any of std430, std140...
-	m_module.decorateArrayStride(arrayId, 16);
-#endif  // PSSL_SPIRV_CROSS_DEBUG
-	
+
+	// spirv-cross doesn't support buffer block expressed as any of std430, std140 and etc.
+	// to use spirv-cross to view the output spv file, enable this and disable above line.
+	// m_module.decorateArrayStride(arrayId, 16);
 
 	uint32_t uboStuctId = m_module.defStructTypeUnique(1, &arrayId);
 	m_module.decorateBlock(uboStuctId);
