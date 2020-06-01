47dc7c4d4a5421b7eeb017b8879004f47ac64220
Asuka <bingkan@outlook.com>
2020-06-01 23:53:55 +0800

    add GnmResourceMemory definition

--- GPCS4/Graphic/Gnm/GnmResourceMemory.cpp
+++ GPCS4/Graphic/Gnm/GnmResourceMemory.cpp
@@ -1,1 +1,40 @@
 #include "GnmResourceMemory.h"
+
+GnmResourceMemory::GnmResourceMemory(void* start, uint32_t size) :
+	m_access(GnmMemoryAccess::None),
+	m_accessCount(0)
+{
+	m_range.start = start;
+	m_range.end   = reinterpret_cast<uint8_t*>(start) + size;
+}
+
+GnmResourceMemory::~GnmResourceMemory()
+{
+}
+
+const GnmMemoryRange& GnmResourceMemory::range() const
+{
+	return m_range;
+}
+
+GnmMemoryAccess GnmResourceMemory::access() const
+{
+	return m_access;
+}
+
+void GnmResourceMemory::setAccess(GnmMemoryAccess access)
+{
+	m_access = access;
+	++m_accessCount;
+}
+
+bool GnmResourceMemory::forceUpdate()
+{
+	return m_accessCount >= ForceUpdateCount;
+}
+
+void GnmResourceMemory::reset()
+{
+	m_access = GnmMemoryAccess::None;
+	m_accessCount = 0;
+}
--- GPCS4/Graphic/Gnm/GnmResourceMemory.h
+++ GPCS4/Graphic/Gnm/GnmResourceMemory.h
@@ -2,13 +2,39 @@
 
 #include "GnmCommon.h"
 
-
-/**
- * \brief Simple memory range representation
- */
-struct GnmMemoryRange
+enum class GnmMemoryAccess : uint32_t
 {
-	void* start;
-	void* end;
+	None,  // Haven't been accessed yet.
+	Read,
+	Write
 };
 
+struct GnmMemoryRange
+{
+	void* start = nullptr;
+	void* end   = nullptr;
+};
+
+class GnmResourceMemory
+{
+	constexpr static uint32_t ForceUpdateCount = 10;
+
+public:
+	GnmResourceMemory(void* start, uint32_t size);
+	~GnmResourceMemory();
+
+	const GnmMemoryRange& range() const;
+
+	GnmMemoryAccess access() const;
+
+	void setAccess(GnmMemoryAccess access);
+
+	bool forceUpdate();
+
+	void reset();
+
+private:
+	GnmMemoryRange  m_range;
+	GnmMemoryAccess m_access;
+	uint32_t        m_accessCount;
+};
