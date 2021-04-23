0e26810ef63c7ef33626352d82e39240a3fa84f5
liuziangexit <liuziang@liuziangexit.com>
2021-04-24 00:30:22 +0800

    1.race condition bug fixed 2.improved performance for x86

--- GPCS4/Util/UtilSync.h
+++ GPCS4/Util/UtilSync.h
@@ -11,38 +11,55 @@
  * protect data structures for a short duration
  * in case the structure is not likely contested.
  */
-class Spinlock 
-{
+
+#ifdef _M_X64
+#define __UTIL_SYNC_X86__ 1
+#elif _M_IX86
+#define __UTIL_SYNC_X86__ 1
+#elif __x86_64
+#define __UTIL_SYNC_X86__ 1
+#elif __i386
+#define __UTIL_SYNC_X86__ 1
+#endif
+
+#ifdef __UTIL_SYNC_X86__
+void pause() {
+    //https://software.intel.com/content/www/us/en/develop/articles/benefitting-power-and-performance-sleep-loops.html
+    //https://www.felixcloutier.com/x86/pause
+#ifdef _MSC_VER
+    __asm pause
+#else
+    asm volatile("pause");
+#endif
+}
+#else
+void pause() {
+}
+#endif
+
+class Spinlock {
 
 public:
+    Spinlock() = default;
+    Spinlock(const Spinlock &) = delete;
+    Spinlock &operator=(const Spinlock &) = delete;
 
-	Spinlock() { }
-	~Spinlock() { }
+    void lock() {
+        uint32_t expected;
+        while (!m_lock.compare_exchange_weak(expected = 0, 1, std::memory_order_release, std::memory_order_consume)) {
+            pause();
+        }
+    }
 
-	Spinlock(const Spinlock&) = delete;
-	Spinlock& operator = (const Spinlock&) = delete;
+    void unlock() {
+        m_lock.store(0, std::memory_order_release);
+    }
 
-	void lock() 
-	{
-		while (!this->try_lock())
-		{
-			std::this_thread::yield();
-		}
-			
-	}
-
-	void unlock() 
-	{
-		m_lock.store(0, std::memory_order_release);
-	}
-
-	bool try_lock() 
-	{
-		return !m_lock.load() && !m_lock.exchange(1, std::memory_order_acquire);
-	}
+    bool try_lock() {
+        uint32_t expected = 0;
+        return m_lock.compare_exchange_strong(expected, 1, std::memory_order_release, std::memory_order_consume);
+    }
 
 private:
-
-	std::atomic<uint32_t> m_lock = { 0 };
-
+    std::atomic<uint32_t> m_lock = {0};
 };
