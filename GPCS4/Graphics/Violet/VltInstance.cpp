e73871029b25fda83a01dd596a4a2a6b102d2ebf
Asuka <6732524+Inori@users.noreply.github.com>
2022-05-05 04:08:46 +0800

    fix dynamic depth bound test

--- GPCS4/Graphics/Violet/VltInstance.cpp
+++ GPCS4/Graphics/Violet/VltInstance.cpp
@@ -77,10 +77,13 @@ namespace sce::vlt
 
 		VkDebugUtilsMessengerCreateInfoEXT debugInfo = {};
 		debugInfo.sType                              = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-		debugInfo.messageSeverity                    = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-		debugInfo.messageType                        = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-		debugInfo.pfnUserCallback                    = debugCallback;
-		debugInfo.pUserData                          = this;
+		debugInfo.messageSeverity                    = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+													   VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		debugInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+								VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT 
+								/* |VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT*/;
+		debugInfo.pfnUserCallback = debugCallback;
+		debugInfo.pUserData       = this;
 
 #endif
 
